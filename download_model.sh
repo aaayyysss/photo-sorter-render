2c1ca62f87b87f5eefdc704b087cfdeb836f3234
aaayyysss <ammar.suleiman@outlook.de>
2025-08-26 14:24:15 +0200

    Update download_model.sh
    revised

--- download_model.sh
+++ download_model.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 echo "📁 Creating model directory..."
 mkdir -p /root/.insightface/models/buffalo_l
@@ -7,11 +6,11 @@ cd /root/.insightface/models/buffalo_l
 
 echo "📥 Downloading model files..."
 
-gdown https://drive.google.com/uc?id=1lqb107Kp3wDBoiTI0Zmltt0QM_qJ5-Nm
-gdown https://drive.google.com/uc?id=1VHwR1nePjt0nOQXZR5SVPhioHhN91dcJ
-gdown https://drive.google.com/uc?id=185NNgxPoe5XDkCMyWaLkyJeCGHBedo_f
-gdown https://drive.google.com/uc?id=19XQbQzHoMOMqbgv63FBvvdab19twQ7xx
-gdown https://drive.google.com/uc?id=1x_oht716_lkGsM75gHNd9XN2bNpsVynh
-gdown https://drive.google.com/uc?id=12xiMN1lVau9ZSArPsfjNewvpTHx27HJK
+gdown https://drive.google.com/uc?id=1lqb107Kp3wDBoiTI0Zmltt0QM_qJ5-Nm -O config.yaml
+gdown https://drive.google.com/uc?id=1VHwR1nePjt0nOQXZR5SVPhioHhN91dcJ -O det_10g.onnx
+gdown https://drive.google.com/uc?id=185NNgxPoe5XDkCMyWaLkyJeCGHBedo_f -O w600k_r50.onnx
+gdown https://drive.google.com/uc?id=19XQbQzHoMOMqbgv63FBvvdab19twQ7xx -O 1k3d68.onnx
+gdown https://drive.google.com/uc?id=1x_oht716_lkGsM75gHNd9XN2bNpsVynh -O 2d106det.onnx
+gdown https://drive.google.com/uc?id=12xiMN1lVau9ZSArPsfjNewvpTHx27HJK -O genderage.onnx
 
 echo "✅ Model downloaded and saved in /root/.insightface/models/buffalo_l"
