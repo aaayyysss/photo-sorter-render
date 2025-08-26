22ca625d0f05eaaf3b2e15c20d1ef748f4112fc0
aaayyysss <ammar.suleiman@outlook.de>
2025-08-26 12:06:58 +0200

    Update app.py
    if __name__ == "__main__":
        port = int(os.environ.get("PORT", 5000))
        socketio.run(app, host="0.0.0.0", port=port)
    
    to replace:
    if __name__ == "__main__":
        socketio.run(app, host="127.0.0.1", port=5000, debug=True)

--- app.py
+++ app.py
@@ -404,4 +404,5 @@ def write_revert_scripts(manifest, bat_path, sh_path):
         pass
 
 if __name__ == "__main__":
-    socketio.run(app, host="127.0.0.1", port=5000, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host="0.0.0.0", port=port)
